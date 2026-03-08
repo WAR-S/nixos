6b846bb494b6fd5c4b032ce304a0723a437c3bab
Fedorov Vasiliy (Федоров Василий Константинович) <vkfedorov@fil-it.ru>
2026-03-08 15:50:16 +0300

    up version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Minimal edge NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
