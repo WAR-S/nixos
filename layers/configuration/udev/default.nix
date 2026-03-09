70ce28c990cfdc9f7afd5bae3d3c61f9b8664db5
Fedorov Vasiliy (Федоров Василий Константинович) <vkfedorov@fil-it.ru>
2026-03-09 18:30:31 +0300

    udev add

--- layers/configuration/udev/default.nix
+++ layers/configuration/udev/default.nix
@@ -1,9 +1,10 @@
 { ... }:
 
 {
-  # Локальные правила переименования сетевых интерфейсов
-  # будут установлены как /etc/udev/rules.d/10-local.rules
-  environment.etc."udev/rules.d/10-local.rules".text = ''
+  # Локальные правила переименования сетевых интерфейсов.
+  # Используем штатный механизм NixOS — services.udev.extraRules,
+  # он сам положит правила в подходящий файл в /etc/udev/rules.d/.
+  services.udev.extraRules = ''
 SUBSYSTEM=="net", ACTION=="add", KERNEL=="w*", GOTO="wifi_ap"
 SUBSYSTEM=="net", SUBSYSTEMS=="usb", GOTO="handle_usb_modem"
 SUBSYSTEM=="net", SUBSYSTEMS=="pci", GOTO="handle_pci"
