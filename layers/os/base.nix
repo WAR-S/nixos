ab8b6790780407afe3f7aca8f224647ccd5bc326
Fedorov Vasiliy (Федоров Василий Константинович) <vkfedorov@fil-it.ru>
2026-03-10 22:39:41 +0300

    wifi firmware

--- layers/os/base.nix
+++ layers/os/base.nix
@@ -8,4 +8,7 @@
   networking.enableIPv6 = false;
 
   networking.firewall.enable = false;
+
+  # Прошивки для железа (Wi‑Fi iwlwifi, AX200 и т.д.)
+  hardware.enableRedistributableFirmware = true;
 }
