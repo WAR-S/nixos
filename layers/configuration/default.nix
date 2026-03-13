5df16879d396e871c4f734324eddc9af6185dd18
Fedorov Vasiliy (Федоров Василий Константинович) <vkfedorov@fil-it.ru>
2026-03-13 20:06:39 +0300

    nginx-ingress

--- layers/configuration/default.nix
+++ layers/configuration/default.nix
@@ -6,10 +6,11 @@
     ./01-ntp
     ./02-journald
     ./03-hostname
-    ./04-ssh
-    ./05-wifi-ap
-    ./06-postgres
-    ./07-k3s 
+    ./04-resolver
+    ./05-ssh
+    ./06-wifi-ap
+    ./07-postgres
+    ./08-k3s 
     ./banner
   ];
 }
