bfc203676d20c84284af2ee185d9f949432ba09a
Fedorov Vasiliy (Федоров Василий Константинович) <vkfedorov@fil-it.ru>
2026-03-14 15:33:13 +0300

    try to find out with runtime paramethers

--- layers/configuration/08-k3s/default.nix
+++ layers/configuration/08-k3s/default.nix
@@ -5,27 +5,8 @@ let
   k3sCfg = infra.k3s;
   k3sPackage = pkgs.k3s_1_32;
 
-  # Файл кладём вручную через tmpfiles, не через services.k3s.containerdConfigTemplate.
-  containerdConfigFile = pkgs.writeText "k3s-containerd-config.toml.tmpl" ''
-    {{ template "base" . }}
-
-    [plugins."io.containerd.grpc.v1.cri".container_log]
-      max_size = "100m"
-      max_files = 3
-
-    [plugins."io.containerd.grpc.v1.cri".registry.mirrors."insecure-docker-image-name:5000"]
-      endpoint = ["http://insecure-docker-image-name:5000"]
-  '';
-
-  containerdConfigPath = "/var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl";
 in
 {
-  # Файл config.toml.tmpl в каталог k3s (симлинк на store).
-  systemd.tmpfiles.rules = [
-    "d /var/lib/rancher/k3s/agent/etc/containerd 0755 root root -"
-    "L+ ${containerdConfigPath} - - - - ${containerdConfigFile}"
-  ];
-
   services.k3s = {
     enable = true;
     role = "server";
@@ -37,6 +18,8 @@ in
     extraFlags = [
       "--node-external-ip=${apIP}"
       "--resolv-conf=/run/systemd/resolve/resolv.conf"
+      "--kubelet-arg=container-log-max-files=4"
+      "--kubelet-arg=container-log-max-size=50Mi"
     ];
 
     disable = [ "traefik" "coredns"];
